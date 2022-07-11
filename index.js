bceacc8b0ad629ea6457442cc6c67d0a12c6cffb
xauvk <xauvkubalexanderyang@live.com>
2022-07-11 11:16:07 -0400

    add html elements

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+let location = 'London'
 
-fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=London&days=5&aqi=no&alerts=no`)
+fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=5&aqi=no&alerts=no1`)
     .then(resp => resp.json())
     .then(console.log)
