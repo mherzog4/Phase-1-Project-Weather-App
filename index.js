1f7efdbb551b611a8daed881776c768af18a4c00
xauvk <xauvkubalexanderyang@live.com>
2022-07-11 16:06:05 -0400

    added DOMContentLoaded

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-let loc = 'London'
+window.addEventListener('DOMContentLoaded', () => {
+    let loc = 'London'
+    renderWeather()
 
-function renderWeather () {
-    fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=5&aqi=no&alerts=no1`)
+    function renderWeather () {
+        fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=5&aqi=no&alerts=no1`)
         .then(resp => resp.json())
         .then(console.log)
-}
+
+    }
+    
+})
+
