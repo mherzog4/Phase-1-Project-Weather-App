b9d132fdcb41604b2255ed7f6a08d3a02d00f764
xauvk <xauvkubalexanderyang@live.com>
2022-07-11 12:33:15 -0400

    add more html elements

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let location = 'London'
 
-fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=5&aqi=no&alerts=no1`)
-    .then(resp => resp.json())
-    .then(console.log)
+function renderWeather () {
+    fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=5&aqi=no&alerts=no1`)
+        .then(resp => resp.json())
+        .then(console.log)
+}
