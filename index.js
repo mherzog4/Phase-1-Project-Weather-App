449cfc06133a7c2cca0ff129072d0c9737ecd9a0
xauvk <xauvkubalexanderyang@live.com>
2022-07-11 16:24:35 -0400

    addedd Element pointers

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
     let loc = 'London'
     renderWeather()
+    const tempElement = document.querySelector()
+    const iconElement = document.querySelector()
+    const locationElement = document.querySelector()
+    const tElement = document.querySelector()
 
     function renderWeather () {
         fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=5&aqi=no&alerts=no1`)
