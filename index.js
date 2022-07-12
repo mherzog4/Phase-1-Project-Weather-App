dd108f50b237b8597ef597078c9230e86e17efd5
Diana Gelinas <dianablythegelinas@gmail.com>
2022-07-12 12:19:40 -0600

    test commit on Diana's end

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ window.addEventListener('DOMContentLoaded', () => {
     const descriptionElement = document.querySelector('.tempature-descrption')
     const formElement = document.querySelector('.form')
     const locationElement = document.querySelector(".location-timezone")
-    
     formElement.addEventListener("submit", (e) => {
         e.preventDefault()
         renderWeather(e.target.submitField.value)
     })
-
     renderWeather('London')
-
     function renderWeather (loc) {
         fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=5&aqi=no&alerts=no1`)
             .then(resp => resp.json())
@@ -18,10 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 degreeElement.textContent = weatherObj['current']['temp_f']
                 descriptionElement.textContent = weatherObj['current']['condition']['text']
                 locationElement.textContent = weatherObj['location']['name'] + ', ' + weatherObj['location']['region'] + ', ' + weatherObj['location']['country']
-
             })
     }
-
-    
 })
 
