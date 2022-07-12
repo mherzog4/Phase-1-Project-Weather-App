9f996ebbf57e4ad3d4611f586c237d89125c69e1
xauvk <xauvkubalexanderyang@live.com>
2022-07-12 10:15:36 -0400

    adding some JavaScript

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    renderWeather('London')
-})
-
-const degreeElement = document.querySelector(".tempature-degree")
+const degreeElement = document.querySelector('.tempature-degree')
 const descriptionElement = document.querySelector(".tempature-description")
 const formElement = document.querySelector(".form")
+ console.log(degreeElement)
+
+window.addEventListener('DOMContentLoaded', () => {
+   
+    renderWeather('London')
+})
 
 function renderWeather (loc) {
     fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=5&aqi=no&alerts=no1`)
         .then(resp => resp.json())
         .then(weatherObj => {
-            degreeElement = weatherObj.current.temp_f
+            degreeElement.textContent = weatherObj['current']['temp_f']
         })
 }
 
