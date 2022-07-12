b13822aca55f30c9293aec033ccb39601dda485d
xauvk <xauvkubalexanderyang@live.com>
2022-07-12 09:46:40 -0400

    adding sumbit event listener

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
     renderWeather('London')
-    const degreeElement = document.querySelector(".tempature-degree")
-    const tElement = document.querySelector(".tempature-description")
-
-    
-    function renderWeather (loc) {
-        fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=5&aqi=no&alerts=no1`)
-            .then(resp => resp.json())
-            .then(console.log)
-
-    }
-
-    
-    
 })
 
+const degreeElement = document.querySelector(".tempature-degree")
+const descriptionElement = document.querySelector(".tempature-description")
+const formElement = document.querySelector(".form")
 
+function renderWeather (loc) {
+    fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=5&aqi=no&alerts=no1`)
+        .then(resp => resp.json())
+        .then(weatherObj => {
+            degreeElement = weatherObj.current.temp_f
+        })
+}
+
+formElement.addEventListener("submit", (e) => {
+    
+})
