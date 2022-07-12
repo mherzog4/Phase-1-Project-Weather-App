412a3d1cfb0219a0b9e25d063055517f698fcf21
xauvk <xauvkubalexanderyang@live.com>
2022-07-12 09:29:58 -0400

    editing javascript

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
-    let loc = 'London'
-    renderWeather()
-    const tempElement = document.querySelector()
-    const iconElement = document.querySelector()
-    const locationElement = document.querySelector()
-    const tElement = document.querySelector()
+    renderWeather('London')
+    const degreeElement = document.querySelector(".tempature-degree")
+    const tElement = document.querySelector(".tempature-description")
 
-    function renderWeather () {
+    
+    function renderWeather (loc) {
         fetch (`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=5&aqi=no&alerts=no1`)
-        .then(resp => resp.json())
-        .then(console.log)
+            .then(resp => resp.json())
+            .then(console.log)
 
     }
+
+    
     
 })
+
 
